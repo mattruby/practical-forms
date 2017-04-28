120e55115e9896b7385d87b105bbd212c9034bab
Matt Ruby <mattruby@gmail.com>
2017-04-28 12:33:30 -0500

    More simple examples

--- basic-form-renderers/controlled-input/demo.js
+++ basic-form-renderers/controlled-input/demo.js
@@ -1,8 +1,10 @@
 // Note the Provider and color
 var formGroupRenderer = function formGroupRenderer () {
-	return React.createElement('div', {className: 'form-group'},
-		React.createElement('label', {htmlFor: 'emailInput'}, 'Email address'),
-		React.createElement('input', {type: 'email', id: 'emailInput'})
+	return React.createElement('form', null,
+		React.createElement('div', {className: 'form-group'},
+			React.createElement('label', {htmlFor: 'emailInput'}, 'Email address'),
+			React.createElement('input', {className: 'form-control', type: 'email', id: 'emailInput'})
+		)
 	);
 };
 
